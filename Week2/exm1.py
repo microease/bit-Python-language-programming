988fe31993d9265a3c97afb9638997c96281fa83
浮生若梦 <microease@163.com>
2019-07-24 21:05:16 +0800

    add

--- Week2/exm1.py
+++ Week2/exm1.py
@@ -1,10 +1,2 @@
-#请在横线中填写Python表达式或语句，实现所需要的功能
-#注意：补充代码将以匹配方式评阅，代码中不要出现空格
-
-import turtle as t
-t.pensize(2)
-for i in range(8):
-    t.fd(100)
-    t.left(45)
-
-print(3, 4)
+name="Python语言程序设计课程"
+print(name[0],name[2:-2],name[-1])
